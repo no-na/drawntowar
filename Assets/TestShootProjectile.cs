a01329a29948bd88db5d3c707ec782f0f915697d
no-na <noahknam@gmail.com>
2017-08-08 03:15:09 -0400

    Added graph paper background. Added crossbowman prefab. Increased camera size (to make units look smaller). Cleaned up project folders a bit.

---
+++ Assets/TestShootProjectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestShootProjectile : MonoBehaviour {
+	
+	public Animator fireAnimator;
+	public Rigidbody2D projectile;
+
+	// Use this for initialization
+	void Start () {
+		fireAnimator = transform.GetChild(1).GetChild(0).GetComponent<Animator>();
+		projectile = transform.GetChild(2).GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(1)){
+			fireAnimator.SetTrigger("fire");
+			projectile.velocity = Vector2.up * 8f;
+		}
+	}
+}
